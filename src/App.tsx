229afbd53408ecf28878316a8cbb1a0ccc994cd7
yebrai <ivansevv@gmail.com>
2023-04-09 13:54:50 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App () {
+  return (
+    <div className="App">
+      <h1>
+        Google Translate
+      </h1>
+    </div>
+  )
+}
+
+export default App
