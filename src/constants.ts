00e25db75e47e9e829a369cfdcc273798366c3b1
yebrai <ivansevv@gmail.com>
2023-04-17 13:02:34 +0200

    added new languages

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,10 @@
 export const SUPPORTED_LANGUAGES = {
   en: 'English',
   es: 'Español',
-  de: 'Deustch'
+  de: 'Deustch',
+  fr: 'French',
+  it: 'Italian',
+  ja: 'Japanese'
 }
 
 export const AUTO_LANGUAGE = 'auto'
