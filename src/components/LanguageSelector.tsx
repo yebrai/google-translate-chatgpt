fdb395f96167a428b711e42a13cf8dd0353a8b59
yebrai <ivansevv@gmail.com>
2023-04-18 16:44:43 +0200

    fixing github deploy fail

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -4,8 +4,8 @@ import { type FC } from 'react'
 import { type SectionType, type FromLanguage, type Language } from '../types'
 
 type Props =
-| { type: SectionType.From, value: FromLanguage, onChange: (language: FromLanguage) => void }
-| { type: SectionType.To, value: Language, onChange: (language: Language) => void }
+  | { type: SectionType.From, value: FromLanguage, onChange: (language: FromLanguage) => void }
+  | { type: SectionType.To, value: Language, onChange: (language: Language) => void }
 
 export const LanguageSelector: FC<Props> = ({ onChange, type, value }) => {
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -13,14 +13,14 @@ export const LanguageSelector: FC<Props> = ({ onChange, type, value }) => {
   }
 
   return (
-        <Form.Select aria-label='Selecciona el idioma' onChange={handleChange} value={value}>
-            {type === 'from' && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
-            {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
-                <option key={key} value={key}>
-                    {literal}
-                </option>
-            ))}
+    <Form.Select aria-label='Selecciona el idioma' onChange={handleChange} value={value}>
+      {type === 'from' && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
+      {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
+        <option key={key} value={key}>
+          {literal}
+        </option>
+      ))}
 
-        </Form.Select>
+    </Form.Select>
   )
 }
