b0ff72303772837c52374f4704f6d7d78cfedbf7
yebrai <ivansevv@gmail.com>
2023-04-18 18:01:35 +0200

    fixing github deploy fail

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'google-translate-chatgpt',
+  base: '/google-translate-chatgpt/',
   test: {
     environment: 'happy-dom'
   }
