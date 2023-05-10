bee1a9113a132a1cc72301d726d6cc95d8af43c9
yebrai <ivansevv@gmail.com>
2023-05-10 13:31:38 +0200

    added error msg

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App () {
       .catch(() => { setResult('API requests passed, try again later.') })
   }, [debouncedFromText, fromLanguage, toLanguage])
 
-  const handleClipboard = () => { navigator.clipboard.writeText(result).catch(() => {}) }
+  const handleClipboard = () => { navigator.clipboard.writeText(result).catch(() => { }) }
   const handleSpeak = () => {
     const utterance = new SpeechSynthesisUtterance(result)
     utterance.lang = toLanguage
@@ -65,26 +65,26 @@ function App () {
               value={toLanguage}
             />
             <div style={{ position: 'relative' }}>
-                          <TextArea
-              loading={loading}
-              type={SectionType.To}
-              value={result}
-              onChange={setResult}
-            />
-            <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
-            <Button
-            variant='link'
-            onClick={handleClipboard}
-            >
-              <ClipboardIcon />
-            </Button>
-            <Button
-            variant='link'
-            onClick={handleSpeak}
-            >
-              <SpeakerIcon />
-            </Button>
-            </div>
+              <TextArea
+                loading={loading}
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+              />
+              <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
+                <Button
+                  variant='link'
+                  onClick={handleClipboard}
+                >
+                  <ClipboardIcon />
+                </Button>
+                <Button
+                  variant='link'
+                  onClick={handleSpeak}
+                >
+                  <SpeakerIcon />
+                </Button>
+              </div>
 
             </div>
 
