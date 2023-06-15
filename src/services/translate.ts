96a2b54db71428bd5d3a9d46f8856e4f3e4d1755
yebrai <ivansevv@gmail.com>
2023-06-15 18:33:08 +0200

    solved with mymemory api

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -14,7 +14,7 @@ export async function translate ({
   if (text === '') return null
   const res = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${fromLanguage}|${toLanguage}`)
   const data = await res.json()
-  // console.log(data)
+
   return data.responseData.translatedText
 }
 
